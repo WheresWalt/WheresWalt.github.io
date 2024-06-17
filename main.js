9e9a6e8fcd668e10cf717cc88e0b7f4d87085f1a
wwdeveas <wwdeveas@my.loyno.edu>
2024-06-16 19:30:44 -0500

    upd

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-let map;
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-initMap();
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
---
+++ meme.py
@@ -0,0 +1,3 @@
+import random
+arr = ["movie", "snakes (aka wait)", ""]
+print(arr[random.randrange(0, 3)])
